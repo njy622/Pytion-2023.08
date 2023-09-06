5e188daf4a0617d160d7ee6322c7e6ed492419de
njy622 <njy622@naver.com>
2023-09-06 12:49:16 +0900

    파이썬 객체

---
+++ 08.객체지향/account2.py
@@ -0,0 +1,72 @@
+"""
+### class Account (계좌)를 만드세요.
+1. 속성값 : 
+- 계좌번호(ano), 소유주(owner), 잔액(balance)
+- 단, 잔액은 0월 이상이고, 10,000,000원 이하이어야 한다.
+
+2. deposit method:
+- amount를 매개변수로 받아 balance에 더해준다.
+- 잔액은 10000000원을 초과할 수 없다.
+
+3. withdraw method:
+- amount 만큼의 금액을 balance에서 차감한다.
+- 잔액은 마이너스 금액이 될 수 없다.
+
+4. __str__ method:
+- 계좌번호: 000, 소유주: 000, 잔액: 10자리의 천단위 구분기호가 있는 정수
+"""
+## 선생님 풀이
+class Account:
+    def __init__(self, ano, owner, balance):
+        self.ano = ano
+        self.owner = owner
+        self.__balance = 0
+        if 0 <= balance <= 10000000:
+            self.__balance = balance
+        #else:
+        #   self.balance = 0
+    
+    def deposit(self, amount):
+        if self.__balance + amount > 10000000:
+            print('일천만원이 초과할 수 없습니다.')
+            return
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        #if self.balance - amount < 0:
+        if self.__balance < amount:
+            print('잔액이 부족합니다.')
+            return
+        self.__balance -= amount
+
+    def __str__ (self):
+        return f'계좌번호: {self.ano}, 소유주: {self.owner}, 잔액:{self.__balance:10,d}'
+    
+  
+if __name__ == '__main__':
+    acc = Account('230906', '제임스', 100000)
+    print(acc)
+    acc.deposit(200000)
+    print(acc)
+    acc.withdraw(400000)
+    acc.withdraw(250000)
+    print(acc)
+
+
+"""
+CMD 창에서 조회할때,
+
+d:
+cd Workspace\01.Python\08.객체지향
+conda activate base
+python account.py
+
+이렇게 치면
+아래와 같이 실행됨
+
+계좌번호: 230906, 소유주: 제임스, 잔액:   100,000
+계좌번호: 230906, 소유주: 제임스, 잔액:   300,000
+잔액이 부족합니다.
+계좌번호: 230906, 소유주: 제임스, 잔액:    50,000
+
+"""
---
+++ 08.객체지향/bank_app.py
@@ -0,0 +1,35 @@
+import os, sys, joblib
+import bank_util as bu
+from account import Account
+
+account_filename = 'account.jl'
+if os.path.exists(account_filename):            # 계좌피일이 있으면
+    acc_list = joblib.load(account_filename)    # 계좌리스트를 파일에 로드하고
+else:                                           # 계좌파일이 없으면
+    acc_list = []                               # 빈리스트
+
+while True:
+
+    try:
+        menu = int(input('1:계좌생성, 2:계좌목록, 3:입금, 4:출금, 5:종료> '))
+    except:
+        print('잘못된 명령입니다. 1~5 사이의 숫자를 입력하세요 \n')
+        continue
+
+    if menu == 1:
+        bu.create_account(acc_list)
+    elif menu == 2:
+        for acc in acc_list:
+            print(acc)
+    elif menu == 3:
+        bu.deposit(acc_list)
+    elif menu == 4:
+        bu.withdraw(acc_list)
+    elif menu == 5:
+        joblib.dump(acc_list, account_filename)  
+        # 계좌 리스트를 종료시에 파일에 저장
+        sys.exit()
+    else:
+        print('잘못된 명령입니다.\n')
+
+    print()
